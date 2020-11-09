f82784c328c96969cbcfe0d589078d91e5ac965a
Arto <70948173+Arto67@users.noreply.github.com>
2020-11-09 08:50:45 +0200

    Add files via upload
    Removed hours and minutes display, a display for stop time and made buttons invisible (1,2,4,8,0 "reset")

--- Stopwatch.js
+++ Stopwatch.js
@@ -5,7 +5,10 @@ class Stopwatch extends Component {
         state = {
           timerOn: false,
           timerStart: 0,
-          timerTime: 0
+          timerTime: 0,
+          preSetTime: 0,
+          preSetTimer: 0,
+          preSetText: "00:00:00"
         };    
  
   startTimer = () => {
@@ -20,49 +23,85 @@ class Stopwatch extends Component {
           });
         }, 10);
       };
-      stopTimer = () => {
-        this.setState({
-          timerTime: 10000
-        });
-        this.setState({ timerOn: false });
-        clearInterval(this.timer);
-      };resetTimer = () => {
-        this.setState({
-          timerStart: 0,
-          timerTime: 0
-        });
-      };
 
-    render() {
-      const { timerTime } = this.state;
-      let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
-      let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-      let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
-      let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
-      return (
-            <div className="Stopwatch-display">
-                  {hours} : {minutes} : {seconds} : {centiseconds}
-                  
-                  {this.state.timerOn === false && this.state.timerTime === 0 && (
-                  <button onClick={this.startTimer}>Start</button>
-                  )}
-                  {this.state.timerOn === true && (
-                    <button onClick={this.stopTimer}>Stop</button>
-                  )}
-                  {this.state.timerOn === false && this.state.timerTime > 0 && (
-                    <button onClick={this.startTimer}>Resume</button>
-                  )}
-                  {this.state.timerOn === false && this.state.timerTime > 0 && (
-                    <button onClick={this.resetTimer}>Reset</button>
-                  )}
-                  <button>+1</button>
-                  <button>+2</button>
-                  <button>+4</button>
-                  <button>+8</button>
+  stopTimer = () => {
+    this.state.preSetTimer !== 0 && this.setState({ timerTime: this.state.preSetTimer });
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+  };
+  
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0
+    });
+  };
 
-            </div>
-      );
+  setStateSync(state){
+    return new Promise(resolve=>{
+        this.setState(state,resolve)
+    })
+  };
+  async setPreSetTime(snglBtn){
+    if (snglBtn === "reset") {
+      await this.setStateSync({
+        preSetTime: 0,
+        preSetTimer: 0 
+      })
+    } else {
+      await this.setStateSync({
+        preSetTime: parseInt(this.state.preSetTime + snglBtn.toString()),
+        preSetTimer: parseInt(this.state.preSetTime.toString() + snglBtn.toString() + "0")
+      })
     }
-  }
+    const { preSetTimer } = this.state;
+    let centiseconds = ("0" + (Math.floor(preSetTimer / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(preSetTimer / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(preSetTimer / 60000) % 60)).slice(-2);
+    let timeSet = minutes + ":" + seconds + ":" + centiseconds;
+    this.setState({preSetText: timeSet});
+    console.log ( this.state.preSetTimer )
+  };
 
-  export default Stopwatch;
+  render() {
+    
+    const btnList = [1,2,4,8,0,"reset"];
+    
+    const timeBtns = btnList.map((snglBtn) =>
+      <button key={snglBtn.toString()} onClick={() => this.setPreSetTime(snglBtn)}>{snglBtn}</button>
+    );
+
+    const { timerTime } = this.state;
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+    return (
+      <div className="Stopwatch">
+          <div className="Stopwatch-display">
+                {seconds} : {centiseconds}
+                <div>
+                {this.state.timerOn === false && this.state.timerTime === 0 && (
+                <button onClick={this.startTimer}>Start</button>
+                )}
+                {this.state.timerOn === true && (
+                  <button onClick={this.stopTimer}>Stop</button>
+                )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                  <button onClick={this.startTimer}>Resume</button>
+                )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                  <button onClick={this.resetTimer}>Reset</button>
+                )}
+                </div>
+                <br />
+                {/* <p>Pre-Set Time</p> */}
+                {/* {this.state.preSetText} */}
+                <div className="Numpad">{timeBtns}</div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Stopwatch;
